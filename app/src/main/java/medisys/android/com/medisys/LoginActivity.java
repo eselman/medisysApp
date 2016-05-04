5159b23d66fb23cdc8452f5cd600fd781b77733a
Evangelina Selman <eselman@belatrixsf.com>
2016-05-04 18:22:13 -0300

    Adding Menu Activity

--- app/src/main/java/medisys/android/com/medisys/LoginActivity.java
+++ app/src/main/java/medisys/android/com/medisys/LoginActivity.java
@@ -1,13 +1,14 @@
 package medisys.android.com.medisys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -56,22 +57,11 @@ public class LoginActivity extends AppCompatActivity implements LoginClientTask.
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void loginCallback(Object sessionKey) {
-        username.setText(sessionKey.toString());
+        Log.v("MEDISYS", "Session Key: " + sessionKey);
+        if (sessionKey != null) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/medisys/android/com/medisys/MenuActivity.java
@@ -0,0 +1,39 @@
+package medisys.android.com.medisys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar menuToolbar = (Toolbar) findViewById(R.id.menu);
+        setSupportActionBar(menuToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_patients:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Log.i("MEDISYS", "Seleccione la opción de pacientes");
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+            }
+        }
+    }
